38b614d9a1de7fb1b66ff6af5a10499d650afd45
Jérémy CASTELLANO <jeremy.castellano@me.com>
2022-10-31 17:59:22 +0100

    feature(user): added CreateUser and UpdateUser methods

--- user.go
+++ user.go
@@ -1,7 +1,11 @@
 package aircallgo
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -25,6 +29,20 @@ type Users struct {
 	Users []User `json:"users"`
 }
 
+type createUserPayload struct {
+	email              string
+	firstName          string
+	lastName           string
+	availabilityStatus string
+	isAdmin            bool
+}
+
+type updateUserPayload struct {
+	firstName          string
+	lastName           string
+	availabilityStatus string
+}
+
 func GetUser(UserID int64) *User {
 	client := (*Client[User])(newClient())
 	user := client.MakeRequest("users/"+strconv.FormatInt(UserID, 10), http.MethodGet, nil)
@@ -35,4 +53,60 @@ func GetUsers() *Users {
 	client := (*Client[Users])(newClient())
 	users := client.MakeRequest("users", http.MethodGet, nil)
 	return &users
+}
+
+func CreateUser(Email string, FirstName string, LastName string, AvailabilityStatus string, IsAdmin bool) *User {
+	if len(Email) == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(CreateUser): Email cannot be empty")
+		return nil
+	}
+	if len(FirstName) == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(CreateUser): FirstName cannot be empty")
+		return nil
+	}
+	if len(LastName) == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(CreateUser): LastName cannot be empty")
+		return nil
+	}
+	if !isValidAvailabilityStatus(AvailabilityStatus) {
+		AvailabilityStatus = "available"
+	}
+	client := (*Client[User])(newClient())
+	payload, err := json.Marshal(createUserPayload{
+		email:              Email,
+		firstName:          FirstName,
+		lastName:           LastName,
+		availabilityStatus: AvailabilityStatus,
+		isAdmin:            IsAdmin,
+	})
+	if err != nil {
+		fmt.Fprint(os.Stderr, "aircallgo(CreateUser): unable to marshal json payload")
+		panic(err)
+	}
+	r := bytes.NewReader(payload)
+	user := client.MakeRequest("users", http.MethodPost, r)
+	return &user
+}
+
+func UpdateUser(UserID int64, FirstName string, LastName string, AvailabilityStatus string) *User {
+	if UserID == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(UpdateUser): target user ID cannot be 0")
+		return nil
+	}
+	if !isValidAvailabilityStatus(AvailabilityStatus) {
+		AvailabilityStatus = "available"
+	}
+	client := (*Client[User])(newClient())
+	payload, err := json.Marshal(updateUserPayload{
+		firstName:          FirstName,
+		lastName:           LastName,
+		availabilityStatus: AvailabilityStatus,
+	})
+	if err != nil {
+		fmt.Fprint(os.Stderr, "aircallgo(CreateUser): unable to marshal json payload")
+		panic(err)
+	}
+	r := bytes.NewReader(payload)
+	user := client.MakeRequest("users/"+strconv.FormatInt(UserID, 10), http.MethodPut, r)
+	return &user
 }
