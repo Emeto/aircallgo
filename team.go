04612ae2f0f12ab0f80fd9614807dee3cfa26c69
Jérémy Castellano <jeremy.castellano@me.com>
2022-11-04 21:58:03 +0100

    feature(team): added GetTeams & GetTeam methods

--- team.go
+++ team.go
@@ -1,6 +1,10 @@
 package aircallgo
 
-import "time"
+import (
+	"net/http"
+	"strconv"
+	"time"
+)
 
 type Team struct {
 	ID         int64     `json:"id"`
@@ -8,4 +12,21 @@ type Team struct {
 	Name       string    `json:"name"`
 	CreatedAt  time.Time `json:"created_at"`
 	Users      []User    `json:"users"`
+}
+
+type Teams struct {
+	Meta  Meta   `json:"meta"`
+	Teams []Team `json:"teams"`
+}
+
+func GetTeams() *Teams {
+	client := (*Client[Teams])(newClient())
+	teams := client.MakeRequest("teams", http.MethodGet, nil)
+	return &teams.data
+}
+
+func GetTeam(TeamID int64) *Team {
+	client := (*Client[Team])(newClient())
+	team := client.MakeRequest("teams/"+strconv.FormatInt(TeamID, 10), http.MethodGet, nil)
+	return &team.data
 }
