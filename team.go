8a4a2fa9aa973eef40bd8eb76cd8a777f92179ad
Jérémy Castellano <jeremy.castellano@me.com>
2022-11-10 11:31:32 +0000

    feature(team): added DeleteTeam, AddUserToTeam, DeleteUserFromTeam methods

--- team.go
+++ team.go
@@ -1,7 +1,9 @@
 package aircallgo
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -26,7 +28,49 @@ func GetTeams() *Teams {
 }
 
 func GetTeam(TeamID int64) *Team {
+	if TeamID == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(GetTeam): target team ID cannot be 0")
+		return nil
+	}
 	client := (*Client[Team])(newClient())
 	team := client.MakeRequest("teams/"+strconv.FormatInt(TeamID, 10), http.MethodGet, nil)
 	return &team.data
+}
+
+func DeleteTeam(TeamID int64) bool {
+	if TeamID == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(DeleteTeam): target team ID cannot be 0")
+		return false
+	}
+	client := newClient()
+	response := client.MakeRequest("teams/"+strconv.FormatInt(TeamID, 10), http.MethodDelete, nil)
+	return response.StatusCode == http.StatusOK
+}
+
+func AddUserToTeam(TeamID int64, UserID int64) *Team {
+	if TeamID == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(AddUserToTeam): target team ID cannot be 0")
+		return nil
+	}
+	if UserID == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(AddUserToTeam): target user ID cannot be 0")
+		return nil
+	}
+	client := (*Client[Team])(newClient())
+	response := client.MakeRequest("teams/"+strconv.FormatInt(TeamID, 10)+"/users/"+strconv.FormatInt(UserID, 10), http.MethodPost, nil)
+	return &response.data
+}
+
+func DeleteUserFromTeam(TeamID int64, UserID int64) *Team {
+	if TeamID == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(DeleteUserFromTeam): target team ID cannot be 0")
+		return nil
+	}
+	if UserID == 0 {
+		fmt.Fprint(os.Stderr, "aircallgo(DeleteUserFromTeam): target user ID cannot be 0")
+		return nil
+	}
+	client := (*Client[Team])(newClient())
+	response := client.MakeRequest("teams/"+strconv.FormatInt(TeamID, 10)+"/users/"+strconv.FormatInt(UserID, 10), http.MethodDelete, nil)
+	return &response.data
 }
